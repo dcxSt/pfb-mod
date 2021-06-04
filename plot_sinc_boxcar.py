12c75419763dec98130a48025ede956fa5a4ae74
dcxSt <dcxstephen@live.co.uk>
2021-06-04 12:40:44 -0500

    refactor

--- plot_sinc_boxcar.py
+++ plot_sinc_boxcar.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fft import fft,ifft,fftshift
+from constants import *
 
 
-ntap,nchan=4,1024
 k = 0.0003663 # the boundary of nice sinc is between 0.0003663 and 0.0003662
-boxcar = np.concatenate([np.zeros(int(2*ntap*nchan*((1-k)/2))),np.ones(int(2*ntap*nchan*k)),np.zeros(int(2*ntap*nchan*((1-k)/2)))])
+boxcar = np.concatenate([np.zeros(int(NTAP*LBLOCK*((1-k)/2))),np.ones(int(NTAP*LBLOCK*k)),np.zeros(int(NTAP*LBLOCK*((1-k)/2)))])
 
-sinc = np.sinc(np.linspace(-2,2,2*nchan*ntap))
+sinc = np.sinc(np.linspace(-2,2,2*int(NTAP*LBLOCK/2)))
 
 # plot the windows and their ffts
 plt.subplots(figsize=(14,7))
@@ -16,7 +16,7 @@ plt.plot(boxcar)
 plt.title("Boxcar",fontsize=20)
 
 plt.subplot(2,3,2)
-plt.plot(boxcar[nchan*ntap-4:nchan*ntap+3])
+plt.plot(boxcar[int(NTAP*LBLOCK/2)-4:int(NTAP*LBLOCK/2)+3])
 plt.title("Boxcar Zoom",fontsize=20)
 
 plt.subplot(2,3,3)
@@ -30,9 +30,8 @@ plt.title("Sinc",fontsize=20)
 plt.subplot(2,3,5)
 plt.plot(fftshift(fft(fftshift(sinc))))
 plt.title("(shifted) fft Sinc",fontsize=20)
-
 plt.subplot(2,3,6)
-plt.plot(fftshift(fft(fftshift(sinc)))[nchan*ntap-10:nchan*ntap+10])
+plt.plot(fftshift(fft(fftshift(sinc)))[int(NTAP*LBLOCK/2)-10:int(NTAP*LBLOCK/2)+10])
 plt.title("fft Sinc Zoom",fontsize=20)
 
 plt.tight_layout()
