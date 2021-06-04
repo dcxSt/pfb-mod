5412a1f097f0fdd36a4edb787102680d87c0b746
dcxSt <dcxstephen@live.co.uk>
2021-06-04 12:42:11 -0500

    sinc and sinc_hamming windows moved to helper

--- helper.py
+++ helper.py
@@ -4,10 +4,25 @@ Created on 2021.06.03 16:08:57
 Author : Stephen Fay
 """
 
+import numpy as np
 from scipy.fft import fft,ifft,fftshift,ifftshift
+from constants import *
 
+#%% fourier transforms
 def window_to_box(window):
     return fftshift(fft(fftshift(window))) # gets taller
 
 def box_to_window(box):
-    return ifftshift(ifft(ifftshift(box))) # gets shorter
+    return ifftshift(ifft(ifftshift(box))) # gets smaller
+
+#%% windows 
+
+def sinc_window(ntap=NTAP,lblock=LBLOCK):
+    # i don't like np.arange, it's vulnerable to failur, 
+    # should be linspace, but this is how R.S. implemented it, and it'll give a (tiny bit) different array
+    return np.sinc(np.arange(-ntap/2,ntap/2,1/lblock)) 
+
+def sinc_hamming(ntap=NTAP,lblock=LBLOCK):
+    return np.hamming(ntap*lblock) * sinc_window(ntap,lblock)
+
+    
