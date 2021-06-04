554d164a89a70ca139529a72299f3b30781ec842
dcxSt <dcxstephen@live.co.uk>
2021-06-03 19:58:37 -0500

    created constants.py, stores constants that are used across codebase

---
+++ constants.py
@@ -0,0 +1,19 @@
+# this module is to be imported
+import numpy as np
+from scipy.fft import fftshift,fft
+
+NTAP = 4
+NCHAN = 1024
+LBLOCK = 2*NCHAN
+SINC = np.sinc(np.arange(-NTAP/2,NTAP/2,1/LBLOCK))
+if len(SINC) != NTAP*LBLOCK: raise Exception("incompatible length NTAP, LBLOCK")
+BOXCAR_0 = fftshift(fft(fftshift(SINC)))
+PI = np.pi
+
+# useful for quantization loss optimizations
+SINPI = 0
+SIN2PI = 0
+SIN3PI = 0
+COSPI = -1
+COS2PI = 1
+COS3PI = -1
