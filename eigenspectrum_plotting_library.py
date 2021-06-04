27c5424b26736827ffbef395a8b9480c1c763b73
dcxSt <dcxstephen@live.co.uk>
2021-06-03 19:58:09 -0500

    updated eigenspectrum plots, for some reason loads of duplicate code was in here, which i deleted

--- eigenspectrum_plotting_library.py
+++ eigenspectrum_plotting_library.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fft import rfft,irfft,fft,ifft,fftshift
+from scipy.fft import rfft,fft,fftshift
+from constants import *
 
 #%% some windows for experimentation
 
-def sinc_window(ntap=4,lblock=2048):
+def sinc_window(ntap=NTAP,lblock=LBLOCK):
     """Sinc window function
     
     Parameters
@@ -22,11 +23,11 @@ def sinc_window(ntap=4,lblock=2048):
     X = np.arange(-ntap/2.0,ntap/2.0,1.0/lblock)
     return np.sinc(X)
 
-def sinc_custom(r,offset=0,ntap=4,lblock=2048):
+def sinc_custom(r,offset=0,ntap=NTAP,lblock=LBLOCK):
     X = np.linspace(-r/2.0+offset,r/2.0+offset,ntap*lblock)
     return np.sinc(X)
 
-def wabble(r=np.pi/4,sigma=0.2,ntap=4,lblock=2048):
+def wabble(r=np.pi/4,sigma=0.2,ntap=NTAP,lblock=LBLOCK):
     sine = np.sin(np.linspace(-r,r,ntap*lblock))
     gauss = gaussian(np.linspace(-1.,1.,ntap*lblock),0,sigma)
     return sine * gauss
@@ -42,7 +43,7 @@ def zero_padding(w2d,n_zeros=1024):
     pad = np.zeros((len(w2d),n_zeros))
     return np.concatenate([w2d,pad],axis=1)
 
-def image_eigenvalues(w,ntap=4,lblock=2048,name=None):
+def image_eigenvalues(w,ntap=NTAP,lblock=LBLOCK,name=None):
     """Images the eigenvalues of a window function
     
     Parameters
@@ -100,64 +101,6 @@ def image_eigenvalues(w,ntap=4,lblock=2048,name=None):
                 ft_abs_ceil[i][j]=1.2
     print(count)
     plt.imshow(ft_abs_ceil.T,aspect="auto")
-    plt.xlabel("sample number",fontsize=16)
-    plt.ylabel("rfft abs",fontsize=16)
-    plt.colorbar()
-    if name:
-        plt.title("PFB Eigenvalues\n{}".format(name),fontsize=18)
-        plt.savefig("./figures/pfb-colorplot-{}-window.png".format(name))
-        np.save("./figures/{}-window".format(name),w)
-    else:
-        plt.title("PFB Eigenvalues",fontsize=18)
-        
-    # plot the boxcar (fft)
-    bc = fftshift(fft(fftshift(w))) # the boxcar transform
-    plt.subplot(2,2,3)
-    plt.plot(bc)
-    plt.title("fft window",fontsize=18)
-    
-    plt.subplot(2,2,4)
-    plt.title("fft window zoom",fontsize=18)
-    plt.plot(bc[int(ntap*lblock/2-10):int(ntap*lblock/2+10)])
-    
-    plt.tight_layout()
-    
-    plt.show()
-    return
-
-def image_eigenvalues(w,ntap=4,lblock=2048,name=None):
-    
-    if ntap*lblock!=len(w):raise Exception("len window incompatible")
-    w2d = chop_win(w,ntap,lblock)
-    w2d_padded = zero_padding(w2d)
-    ft = np.apply_along_axis(rfft,1,w2d_padded)
-    ft_abs = np.abs(ft)
-
-    print("rfft shape and timestream blocked shape",ft.shape,w2d_padded.shape) # sanity check
-    plt.subplots(figsize=(16,11))
-    
-    
-    # plot the window and it's four slices
-    plt.subplot(2,2,1)
-    if ntap==4:
-        chopped = chop_win(w).T
-        plt.plot(chopped[0], alpha=0.5, color="red", label="segment 1")
-        plt.plot(chopped[1], alpha=0.5, color="blue", label="segment 2")
-        plt.plot(chopped[2], alpha=0.5, color="green", label="segment 3")
-        plt.plot(chopped[3], alpha=0.5, color="orange", label="segment 4")
-
-        plt.plot(np.linspace(0,lblock,len(w)),w,"-k",label="full window")
-    else:
-        plt.plot(w,"-k",label="full window")
-    
-#     for i,j in enumerate(chopped):
-#         plt.plot(np.linspace(0,len(w),len(j)),j,alpha=0.5,label="segment {}".format(i+1))
-    if name:
-        plt.title("window {}".format(name),fontsize=18)
-    else:plt.title("window",fontsize=18)
-    plt.legend(bbox_to_anchor=(1, 1), loc=1, borderaxespad=0)
-    plt.subplot(2,2,2)
-    plt.imshow(ft_abs.T,aspect="auto")
     plt.xlabel("sample number",fontsize=16)
     plt.ylabel("rfft abs",fontsize=16)
     plt.colorbar()
