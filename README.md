6295559eeec3a63da868aaacb43d32f6267e8010
dcxSt <dcxstephen@live.co.uk>
2021-06-10 17:02:07 -0500

    added differentiable functions to helper

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pfb-mod
 Modifying the Polyphase Filter Bank to make it robust to quantization effects
+
+Dependencies:
+- Jax
+- Numpy
+- Scipy (``scipy.signal``, ``scopy.fft``, ``scipy.optimize``)
+- Matplotlib
--- helper.py
+++ helper.py
@@ -4,16 +4,58 @@ Created on 2021.06.03 16:08:57
 Author : Stephen Fay
 """
 
-import numpy as np
-from scipy.fft import fft,ifft,fftshift,ifftshift
+# %env JAX_ENABLE_x64=1 # not sure what this does yet, but it might be important
+import jax.numpy as np # differentiable numpy library
+from jax.numpy.fft import fft,ifft,fftshift,ifftshift,rfft,irfft
+from jax import custom_jvp
 from constants import *
 
-#%% fourier transforms
+
+#%% custom differentiable JAX functions, and their derivatives
+
+@custom_jvp
+def exp(x):
+    return np.exp(x)
+
+exp.defjvps(lambda x_dot, primal_out, x: x_dot * np.exp(x))
+
+@custom_jvp
+def ln(x): # assumes x > 0
+    return np.log(x)
+
+ln.defjvps(lambda x_dot, primal_out, x: x_dot / x)
+
+@custom_jvp
+def log10(x): # assumes x > 0
+    return np.log10(x)
+
+log10.defjvps(lambda x_dot, primal_out, x: x_dot / (x*np.log(10)))
+
+
+
+#%% fourier transforms and their inverses
 def window_to_box(window):
     return fftshift(fft(fftshift(window))) # gets taller
 
 def box_to_window(box):
     return ifftshift(ifft(ifftshift(box))) # gets smaller
+
+def window_pad_to_box(window,pad_factor=4.0):
+    # pad the window then fft
+    padded_window = np.concatenate([window,np.zeros(int(len(window)*pad_factor))])
+    return window_to_box(padded_window) 
+
+def box_to_window_pad(large_box,len_win):
+    return box_to_window(large_box)[:len_win]
+
+def window_pad_to_box_rfft(window,pad_factor=4.0):
+    padded_window = np.concatenate([window,np.zeros(int(len(window)*pad_factor))])
+    return rfft(fftshift(padded_window))
+
+def box_to_window_pad_rfft(large_box,len_win):
+    return ifftshift(irfft(large_box))[:len_win]
+
+
 
 #%% basic windows, for all windows see windows.py
 
@@ -41,11 +83,19 @@ def window_to_matrix_eig(w,ntap=NTAP,lblock=LBLOCK):
     w2d_padded = zero_padding(w2d)
     ft = np.apply_along_axis(fft,1,w2d_padded)
     return ft
+
 def matrix_eig_to_window(ft_w2d,ntap=NTAP):
     w2d_padded = np.apply_along_axis(ifft,1,ft_w2d)
     w2d = w2d_padded[:,:ntap]
     return np.real(np.concatenate(w2d.T))
+
 def matrix_eig_to_window_complex(ft_w2d,ntap=NTAP):
     w2d_padded = np.apply_along_axis(ifft,1,ft_w2d)
     w2d = w2d_padded[:,:ntap]
     return np.concatenate(w2d.T)
+
+def r_window_to_matrix_eig(w,ntap=NTAP,lblock=LBLOCK):
+    w2d = chop_win(w,ntap,lblock)
+    w2d_padded = zero_padding(w2d)
+    rft = np.apply_along_axis(rfft,1,w2d_padded)
+    return rft
