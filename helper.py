7126ea886d733286c945ea9a39f53f6e42074fb9
Stephen Fay <steve@Stephens-MacBook-Air.local>
2021-07-16 14:43:17 +0200

    cheb window

--- helper.py
+++ helper.py
@@ -5,7 +5,7 @@ Author : Stephen Fay
 """
 
 # %env JAX_ENABLE_x64=1 # not sure what this does yet, but it might be important
-import jax.numpy as np # differentiable numpy library
+import jax.numpy as jnp # differentiable numpy library
 from jax.numpy.fft import fft,ifft,fftshift,ifftshift,rfft,irfft
 from jax import custom_jvp
 from constants import *
@@ -15,40 +15,40 @@ from constants import *
 
 @custom_jvp
 def exp(x):
-    return np.exp(x)
+    return jnp.exp(x)
 
-exp.defjvps(lambda x_dot, primal_out, x: x_dot * np.exp(x))
+exp.defjvps(lambda x_dot, primal_out, x: x_dot * jnp.exp(x))
 
 @custom_jvp
 def ln(x): # assumes x > 0
-    return np.log(x)
+    return jnp.log(x)
 
 ln.defjvps(lambda x_dot, primal_out, x: x_dot / x)
 
 @custom_jvp
 def ln_safe(x): # assumes x >= 0
-    return np.log(x+10**(-20))
+    return jnp.log(x+10**(-20))
 
 ln_safe.defjvps(lambda x_dot, primal_out, x: x_dot / (x+10**(-20)))
 
 @custom_jvp
 def log10(x): # assumes x > 0
-    return np.log10(x)
+    return jnp.log10(x)
 
-log10.defjvps(lambda x_dot, primal_out, x: x_dot / (x*np.log(10)))
+log10.defjvps(lambda x_dot, primal_out, x: x_dot / (x*jnp.log(10)))
 
 
 @custom_jvp
 def log10_safe(x): # assumes x >= 0, useful when you take the log of array with zeros
-    return np.log10(x+10**(-20)) # can go up to -37 in theory
+    return jnp.log10(x+10**(-20)) # can go up to -37 in theory
 
-log10_safe.defjvps(lambda x_dot, primal_out, x: x_dot / ((x+10**(-20))*np.log(10)))
+log10_safe.defjvps(lambda x_dot, primal_out, x: x_dot / ((x+10**(-20))*jnp.log(10)))
 
 @custom_jvp
 def log10_safe_2(x): # assumes x >= 0
-    return np.log10(x+10**(-10))
+    return jnp.log10(x+10**(-10))
 
-log10_safe_2.defjvps(lambda x_dot, primal_out, x: x_dot / ((x + 10**(-10))*np.log(10)))
+log10_safe_2.defjvps(lambda x_dot, primal_out, x: x_dot / ((x + 10**(-10))*jnp.log(10)))
 
 
 #%% fourier transforms and their inverses
@@ -60,14 +60,14 @@ def box_to_window(box):
 
 def window_pad_to_box(window,pad_factor=4.0):
     # pad the window then fft
-    padded_window = np.concatenate([window,np.zeros(int(len(window)*pad_factor))])
+    padded_window = jnp.concatenate([window,jnp.zeros(int(len(window)*pad_factor))])
     return window_to_box(padded_window) 
 
 def box_to_window_pad(large_box,len_win):
     return box_to_window(large_box)[:len_win]
 
 def window_pad_to_box_rfft(window,pad_factor=4.0):
-    padded_window = np.concatenate([window,np.zeros(int(len(window)*pad_factor))])
+    padded_window = jnp.concatenate([window,jnp.zeros(int(len(window)*pad_factor))])
     return rfft(fftshift(padded_window))
 
 def box_to_window_pad_rfft(large_box,len_win):
@@ -79,19 +79,19 @@ def get_spline_func(window):
     half_box = window_pad_to_box_rfft(window,4.0)
     half_box = abs(half_box)
     width = 13 # the width of the boxcar (yes this is hardcoded, can find with peakfinder too)
-    log_lobes = np.log10(half_box[width:])
+    log_lobes = jnp.log10(half_box[width:])
     x = [] 
     y = [] 
     count = 0
     ll = log_lobes.copy()
     while len(ll)>40:
         y.append(max(ll[:20]))
-        x.append(np.argmax(ll[:20]) + 20*count)
+        x.append(jnp.argmax(ll[:20]) + 20*count)
         ll = ll[20:]
         count += 1
     x = [0] + x + [len(half_box)-1] # add bits at the end and at the begginning to cover full range of sidelobes
     y = [y[0]] + y + [y[-1]] 
-    x,y = np.array(x),np.array(y)
+    x,y = jnp.array(x),jnp.array(y)
     from scipy.interpolate import interp1d
     f = interp1d(x, y, kind='cubic')
     return f
@@ -101,18 +101,32 @@ def get_spline_func(window):
 def get_spline_arr(window):
     f = get_spline_func(window)
     half_box = abs(window_pad_to_box_rfft(window,4.0)[13:])
-    x_new = np.arange(len(half_box)) # add 13 to this to plot with rfft boxcar
+    x_new = jnp.arange(len(half_box)) # add 13 to this to plot with rfft boxcar
     y_new = f(x_new)
     return x_new,y_new
 
 # moving average, k is over how many neighbours, so k=1 will be av over 3 neighbours
 def mav(signal,k=1):
-    s = np.r_[np.ones(k)*signal[0],signal,np.ones(k)*signal[-1]] # pad the signal, extend edge values
-    w = np.ones(2*k+1)
+    s = jnp.r_[jnp.ones(k)*signal[0],signal,jnp.ones(k)*signal[-1]] # pad the signal, extend edge values
+    w = jnp.ones(2*k+1)
     w /= w.sum()
-    return np.convolve(w,s,mode="valid")
+    return jnp.convolve(w,s,mode="valid")
 
 
+#%% Chebyshev window
+def cheb_win(coeffs_tail, len_win):
+    """
+    param coeffs_tail jnp.ndarray : 1d array of chebyshev coefficients, assuems first chebyshev coefficient is 1.0 (the constant term)
+    param len_win int : the length of the SINC window (usually 2048 * 4 I think)
+    """
+    coeffs = jnp.concatenate([jnp.ones(1),coeffs_tail])
+    arr2d = jnp.repeat(jnp.array([jnp.linspace(-1,1,len_win)]),len(coeffs),axis=0)
+    arr2d = list(arr2d)
+    for idx,row in enumerate(arr2d):
+        arr2d[idx] = coeffs[idx] * jnp.cos(idx * row)
+    arr2d = jnp.array(arr2d)
+    cheb_window = jnp.sum(arr2d,axis=0)
+    return cheb_window 
 #%% Metrics for evaluating windows 
 
 def metric_sidelobe_thicknesses(window):
@@ -121,11 +135,11 @@ def metric_sidelobe_thicknesses(window):
     normed_box = window_pad_to_box_rfft(window,pad_factor=4.0) / scale
     log_box_abs = log10(abs(normed_box))
     l = len(log_box_abs)
-    th2 = 100*(max(np.where(log_box_abs > -2)[0]) - 13) / l # 13 is the width of the boxcar
-    th3 = 100*(max(np.where(log_box_abs > -3)[0]) - 13) / l
-    th4 = 100*(max(np.where(log_box_abs > -4)[0]) - 13) / l
-    th5 = 100* max(np.where(log_box_abs > -5)[0]) / l # here boxcar width doesn't really matter anymore
-    th6 = 100* max(np.where(log_box_abs > -6)[0]) / l
+    th2 = 100*(max(jnp.where(log_box_abs > -2)[0]) - 13) / l # 13 is the width of the boxcar
+    th3 = 100*(max(jnp.where(log_box_abs > -3)[0]) - 13) / l
+    th4 = 100*(max(jnp.where(log_box_abs > -4)[0]) - 13) / l
+    th5 = 100* max(jnp.where(log_box_abs > -5)[0]) / l # here boxcar width doesn't really matter anymore
+    th6 = 100* max(jnp.where(log_box_abs > -6)[0]) / l
     return th2,th3,th4,th5,th6
 
 
@@ -135,40 +149,40 @@ def metric_sidelobe_thicknesses(window):
 def sinc_window(ntap=NTAP,lblock=LBLOCK):
     # i don't like np.arange, it's vulnerable to failur, 
     # should be linspace, but this is how R.S. implemented it, and it'll give a (tiny bit) different array
-    return np.sinc(np.arange(-ntap/2,ntap/2,1/lblock)) 
+    return jnp.sinc(jnp.arange(-ntap/2,ntap/2,1/lblock)) 
 
 def sinc_hamming(ntap=NTAP,lblock=LBLOCK):
-    return np.hamming(ntap*lblock) * sinc_window(ntap,lblock)
+    return jnp.hamming(ntap*lblock) * sinc_window(ntap,lblock)
 
 #%% helpers for eigenvalue displays
 def chop_win(w,ntap=4,lblock=2048):
     """Chop lblock bits of len ntap of window to get ready for DFT"""
     if ntap*lblock!=len(w):raise Exception("len window incompatible")
-    return np.reshape(w,(ntap,lblock)).T
+    return jnp.reshape(w,(ntap,lblock)).T
 
 def zero_padding(w2d,n_zeros=1024):
-    pad = np.zeros((len(w2d),n_zeros))
-    return np.concatenate([w2d,pad],axis=1)
+    pad = jnp.zeros((len(w2d),n_zeros))
+    return jnp.concatenate([w2d,pad],axis=1)
 
 
 def window_to_matrix_eig(w,ntap=NTAP,lblock=LBLOCK):
     w2d = chop_win(w,ntap,lblock)
     w2d_padded = zero_padding(w2d)
-    ft = np.apply_along_axis(fft,1,w2d_padded)
+    ft = jnp.apply_along_axis(fft,1,w2d_padded)
     return ft
 
 def matrix_eig_to_window(ft_w2d,ntap=NTAP):
-    w2d_padded = np.apply_along_axis(ifft,1,ft_w2d)
+    w2d_padded = jnp.apply_along_axis(ifft,1,ft_w2d)
     w2d = w2d_padded[:,:ntap]
-    return np.real(np.concatenate(w2d.T))
+    return jnp.real(jnp.concatenate(w2d.T))
 
 def matrix_eig_to_window_complex(ft_w2d,ntap=NTAP):
-    w2d_padded = np.apply_along_axis(ifft,1,ft_w2d)
+    w2d_padded = jnp.apply_along_axis(ifft,1,ft_w2d)
     w2d = w2d_padded[:,:ntap]
-    return np.concatenate(w2d.T)
+    return jnp.concatenate(w2d.T)
 
 def r_window_to_matrix_eig(w,ntap=NTAP,lblock=LBLOCK):
     w2d = chop_win(w,ntap,lblock)
     w2d_padded = zero_padding(w2d)
-    rft = np.apply_along_axis(rfft,1,w2d_padded)
+    rft = jnp.apply_along_axis(rfft,1,w2d_padded)
     return rft
