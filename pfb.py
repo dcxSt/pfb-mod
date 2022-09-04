5aec3afda1b87ef9b779d685304d8cb765ba9404
Stephen Fay <dcxstephen@live.co.uk>
2022-09-04 18:13:23 -0400

    ipfb tidied up comments

--- pfb.py
+++ pfb.py
@@ -105,7 +105,8 @@ def bump_up_zero_values(arr):
     return # don't have to return anything because arrays arr is a pointer
 
 # pseudoinverse pfb
-def inverse_pfb(spec, nchan=1025, ntap=4, window=h.sinc_hanning, wiener_thresh=0.0):
+def inverse_pfb(spec, nchan = 1025, ntap = 4, 
+        window = h.sinc_hanning, wiener_thresh = 0.0):
     """Performs pseudo inverse pfb, assumes circulant boundary conditions
 
     Parameters
@@ -113,58 +114,88 @@ def inverse_pfb(spec, nchan=1025, ntap=4, window=h.sinc_hanning, wiener_thresh=0
     spec : numpy 2d array (pfb channelized output)
         spec.shape = (:,nchan), the first entry is the length of the timestream
     nchan : int 
-        usually 1025, it's the number of output channels
+        Usually 1025, it's the number of output channels
     ntap : int
-        usually 4, it's the number of taps
+        Usually 4, it's the number of taps
     window : function
-        the windowing function to apply, usually a sinc_hanning
+        The windowing function to apply, usually a sinc_hanning
     wiener_thresh : float
-        if 0.0, no wiener filter is applied, if greater than zero, a wiener filter is applied with specified threshold 
-
+        If 0.0 is passed, no wiener filter is applied, if greater than 
+        zero, a wiener filter is applied with specified threshold. The 
+        wiener threshold should be the same as the standard deviation 
+        of the noise?
 
     Returns
     -------
-    timestream : numpy 1d array
-        the pseudo-inverse timestream
+    np.ndarray
+        The pseudo-inverse timestream. A 1d array. 
     """
+    # Checks
+    if spec.shape[1] != nchan: 
+        raise Exception("spec.shape {spec.shape} != nchan {nchan}")
     # number of samples in a block
     lblock = 2*(nchan - 1)
 
-    # sw_ts is what is returnd by applying sw matrix  to the original timstream chunk 
-    # each subarray is like [g(1+4k)w1+g(17+4k)w17, ... ,g(16+2k)w16,g(32+4k)w32] ... i think
-    sw_ts = np.apply_along_axis(irfft,1,spec) # what the ts looks like after applying SW matrices
-    win = window(ntap,lblock) # this should be sing.hanning NOT sinc.hamming, see args
-
-    # # temporary fill a list, change to numpy zeros later 
-    # timestream = [] # np.zeros((lblock,sw_ts.shape[1]*ntap)) # initialize reconstructed timestream
+    # If we represent the PFB as the successive application of three 
+    # linear oprators, FSW, where F is a fourier transform, then 
+    # sw_ts is what is returnd by applying sw matrix to the original 
+    # timestream chunk. The k'th column/subarray will look like this.
+    # 
+    # [ g[k]*w[k]    + g[b+k]*w[b+k]  + g[2b+k]*w[2b+k] + g[3b+k]*w[3b+k], 
+    #   g[b+k]*w[k]  + g[2b+k]*w[b+k] + g[3b+k]*w[2b+k] + g[4b+k]*w[3b+k],
+    #   g[2b+k]*w[k] + g[3b+k]*w[b+k] + g[4b+k]*w[2b+k] + g[5b+k]*w[3b+k],
+    #   g[3b+k]*w[k] + g[4b+k]*w[b+k] + g[5b+k]*w[2b+k] + g[6b+k]*w[3b+k],
+    #   g[4b+k]*w[k] + g[5b+k]*w[b+k] + g[6b+k]*w[2b+k] + g[7b+k]*w[3b+k],
+    #   g[5b+k]*w[k] + g[6b+k]*w[b+k] + g[7b+k]*w[2b+k] + g[8b+k]*w[3b+k],
+    #   g[6b+k]*w[k] + g[7b+k]*w[b+k] + g[8b+k]*w[2b+k] + g[9b+k]*w[3b+k],
+    #   ...
+    # ]
+    # 
+    # Where ntap = 4, and b is lblock
+    sw_ts = np.apply_along_axis(irfft, 1, spec) 
+    # win is the actual array
+    win = window(ntap, lblock) # Should be sinc.hanning NOT sinc.hamming
     nblocks = (sw_ts.shape[0] + ntap - 1)/ntap 
-    if nblocks==int(nblocks):nblocks=int(nblocks)
-    else: raise Exception("nblocks should be an integer!, Something went wrong")
-    timestream = np.zeros((lblock,nblocks*ntap),dtype=np.complex128) 
+    if nblocks == int(nblocks): nblocks = int(nblocks)
+    else: raise Exception("nblocks should be an integer, terminating.")
+    timestream = np.zeros((lblock, nblocks*ntap), dtype=np.complex128) 
 
-    # for loop for now so as not to make it too confusing
+    # Implement as for loop for now so as not to make it too confusing
+    # but in theory we could use np.apply_along_axis and transposes
     for idx,(v,wslice) in enumerate(zip(sw_ts.T , win.reshape(ntap,lblock).T)):
-        wslice_pad = np.roll(np.concatenate([np.flip(wslice),np.zeros(len(v)+ntap-1-len(wslice))]),-3) # flip, pad with zeros, roll
-        # see pfb writup inverting pfb section for why we flip and roll, it's to do with how convolutions are defined
+        # This next line looks like some disgusting python ninja move, 
+        # but it's the cleanest way I could come up with, and it's very 
+        # compact 
+        wslice_pad = np.roll(
+                np.concatenate(
+                    [
+                        np.flip(wslice), 
+                        np.zeros(len(v)+ntap-1-len(wslice))
+                    ]),
+                -3) # flip, pad with zeros, roll
+        # See pfb writup--inverting pfb section for why we flip and 
+        # roll, it's to do with getting the spectrum to allign with the
+        # multiplying array and how convolutions are defined.
 
-        # we can use rfft and irfft instead of fft and ifft, haven't got round to this yet... not sure if it's worth for our purposes really 
+        # We can use rfft and irfft instead of fft and ifft, haven't got round to this yet... not sure if it's worth for our purposes really 
         ft_wslice = fft(wslice_pad) # this is a column of the eigenmatrix
         
-        # make sure there are no zero value, because we are inverting it
+        # Make sure there are no zero values, because we are inverting it
         filt = np.ones(len(ft_wslice))  
         if wiener_thresh > 0.0:  
             filt = np.abs(ft_wslice)**2 / (wiener_thresh**2 + np.abs(ft_wslice)**2) * (1 + wiener_thresh**2)
-            
-        # if there are true zeros in the window slice, make them non zero to avoid divide by zero error, 
-        # there shouldn't be any true zeros from experiance, but it's theoretically possible that there is one, however unlikely
+        # If there are true zeros in the window slice, make them non 
+        # zero to avoid divide by zero error, there shouldn't be any 
+        # true zeros from experiance, but it's theoretically possible 
+        # that there is one, however unlikely.
         if 0.0 in ft_wslice:
-            print("\nin inverse pfb: {} 0.0 values(s) encountered\n".format(len(np.where(ft_wslice == 0.0))))
-            bump_up_zero_values(ft_wslice) # get rid of any zeros, replace with 10**(-100)
+            print("\nWARNING: inverse pfb {} 0.0 values(s) encountered\n".format(len(np.where(ft_wslice == 0.0))))
+            bump_up_zero_values(ft_wslice) # replace zeros with 10^-100
         ft_wslice_recip = 1/ft_wslice
         
-
-        gtilde = ifft(fft(np.concatenate((v,v[:ntap-1])))*filt*ft_wslice_recip) # might be impossible so use rfft and irfft because not 'exactly' real, could use rfft on np.real(thing)
-        # timestream.append(gtilde)# timestream[idx] = gtilde # old
+        # Below, might be impossible so use rfft and irfft because not 
+        # 'exactly' real, could use rfft on np.real(thing)
+        gtilde = ifft(fft(np.concatenate((v,v[:ntap-1]))) * filt * ft_wslice_recip) 
         timestream[idx] = gtilde 
 
 
@@ -173,6 +204,15 @@ def inverse_pfb(spec, nchan=1025, ntap=4, window=h.sinc_hanning, wiener_thresh=0
     # print("timestream shape final ",timestream.shape)
     # timestream = np.array(timestream).T.flatten() # old
 
-    return timestream.T.flatten()   # return the reconstructed timestream
-                                    # the timestream will be returnd with dtype=complex but all the imaginary componants should be 0
+    # Return the reconstructed timestream. The timestream is returned 
+    # as a numpy ndarray with dtype=complex, but all the imaginary 
+    # componants will arise due to edge effects and quantization errors.
+    # In other words, in essence, it is a real array. 
+    return timestream.T.flatten()   
+                                    
+
+
+
+
+
 
