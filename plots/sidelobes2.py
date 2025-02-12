dfb677ea39d6ced8453721ac1ea1c229b9e727de
Stephen Fay <dcxstephen@gmail.com>
2025-02-12 14:26:29 -0500

    sidelobes2.py refactoring and cleanup

--- plots/sidelobes2.py
+++ plots/sidelobes2.py
@@ -11,15 +11,15 @@ def sinc_win(P,N):
     x = (np.arange(0,M)-M/2)/N
     return np.sinc(x)
 
-def get_pow(N, win):
+def get_pow(n, taper):
     delnu = np.linspace(1,0,100)
     pos = np.zeros(NX)
     fcenter = 10
     deltaf = np.arange(-3,5)
-    t = np.linspace(0,1,N)
+    t = np.linspace(0,1,n)
     for i, df in enumerate(deltaf):
         for j,dnu in enumerate(delnu):
-            x=win(N)*np.exp(2j*np.pi*(fcenter+dnu)*t)
+            x=taper(n)*np.exp(2j*np.pi*(fcenter+dnu)*t)
             X=np.fft.fft(x)
 #             print(f"dist k-nu is {df-dnu}")
             pos[i*100+j] = np.abs(X[fcenter+df])**2
@@ -27,35 +27,31 @@ def get_pow(N, win):
             
     return pos
 
-def pfb(x,N,ntap):
-    M=N*ntap
-    s = sinc_win(ntap,N)
-    h = np.hanning(M)
+def pfb(x,nframe,ntap,taper=np.hamming):
+    if taper is None: taper=np.ones
+    h = taper(nframe*ntap)
+    s = sinc_win(ntap,nframe)
     # equivalent
-#     y = s*h*x
-#     y = y.reshape(-1,N,order='c')
-#     y = np.sum(y, axis=0)
-#     X = np.fft.fft(y)
+    # y = s*h*x
+    # y = y.reshape(-1,nframe,order='c')
+    # y = np.sum(y, axis=0)
+    # X = np.fft.fft(y)
     X = np.fft.fft(s*h*x)
     X = X[::ntap].copy()
     return X
 
-def get_pow_pfb(N, ntap):
-    
+def get_pow_pfb(nframe, ntap, taper=np.hamming):
     delnu = np.linspace(1,0,100)
     pos = np.zeros(NX)
     fcenter = 10
     deltaf = np.arange(-3,5)
-    M=N*ntap
-    # Convention: N is for 1 sec. basically N Hz. So n taps in ntap seconds.
+    M=nframe*ntap
     t = np.linspace(0,ntap,M)
     for i, df in enumerate(deltaf):
         for j,dnu in enumerate(delnu):
             x=np.exp(2j*np.pi*(fcenter+dnu)*t)
-            X=pfb(x,N,ntap)
-#             print(f"dist k-nu is {df-dnu}")
+            X=pfb(x,nframe,ntap,taper)
             pos[i*100+j] = np.abs(X[fcenter+df])**2
-#     print(pos)
     return pos
 
 # helper, normalize to decibel scale
@@ -63,12 +59,12 @@ def decibel(y):
     return 10*np.log10(y/y.max())
 
 
-hann=lambda N: np.hanning(N)
-flat=lambda N: np.ones(N)
-pos1=get_pow(1024,flat)
-pos2=get_pow(1024,hann)
-pos3=get_pow_pfb(1024,8)
-pos4=get_pow_pfb(2048,4)
+pos1=get_pow(1024,np.ones)
+pos2=get_pow(1024,np.hamming)
+pos3=get_pow_pfb(1024,8,taper=np.hamming)
+pos4=get_pow_pfb(2048,4,taper=np.hamming)
+
+print(f"Shapes: {pos1.shape}, {pos2.shape}, {pos3.shape}, {pos4.shape}")
 
 xl=np.linspace(-4,4,NX)
 plt.figure(0)
