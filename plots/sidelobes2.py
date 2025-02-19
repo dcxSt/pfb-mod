3bdb547484997723468e2990292a3aed47574e1c
Stephen Fay <dcxstephen@gmail.com>
2025-02-18 17:02:55 -0800

    response function plots tweaked in sidelobes2.py

--- plots/sidelobes2.py
+++ plots/sidelobes2.py
@@ -12,35 +12,30 @@ def sinc_win(P,N):
     return np.sinc(x)
 
 def get_pow(n, taper):
+    """Get the freq response of a taper'd DFT."""
     delnu = np.linspace(1,0,100)
     pos = np.zeros(NX)
     fcenter = 10
     deltaf = np.arange(-3,5)
     t = np.linspace(0,1,n)
+    # For each frequency calculate the response
     for i, df in enumerate(deltaf):
         for j,dnu in enumerate(delnu):
-            x=taper(n)*np.exp(2j*np.pi*(fcenter+dnu)*t)
-            X=np.fft.fft(x)
-#             print(f"dist k-nu is {df-dnu}")
-            pos[i*100+j] = np.abs(X[fcenter+df])**2
-#     print(pos)
-            
+            x=taper(n)*np.exp(2j*np.pi*(fcenter+dnu)*t) # tapered sine wave
+            X=np.fft.fft(x)                             # FFT
+            pos[i*100+j] = np.abs(X[fcenter+df])**2     # power
     return pos
 
 def pfb(x,nframe,ntap,taper=np.hamming):
     if taper is None: taper=np.ones
-    h = taper(nframe*ntap)
-    s = sinc_win(ntap,nframe)
-    # equivalent
-    # y = s*h*x
-    # y = y.reshape(-1,nframe,order='c')
-    # y = np.sum(y, axis=0)
-    # X = np.fft.fft(y)
-    X = np.fft.fft(s*h*x)
-    X = X[::ntap].copy()
+    h = taper(nframe*ntap)      # taper function
+    s = sinc_win(ntap,nframe)   # sinc function
+    X = np.fft.fft(s*h*x)       # FFT
+    X = X[::ntap].copy()        # decimate (could chop b4 FFT too)
     return X
 
-def get_pow_pfb(nframe, ntap, taper=np.hamming):
+def get_pow_pfb(nframe, ntap, taper=np.hanning):
+    """Get the frequency response of a PFB."""
     delnu = np.linspace(1,0,100)
     pos = np.zeros(NX)
     fcenter = 10
@@ -49,9 +44,9 @@ def get_pow_pfb(nframe, ntap, taper=np.hamming):
     t = np.linspace(0,ntap,M)
     for i, df in enumerate(deltaf):
         for j,dnu in enumerate(delnu):
-            x=np.exp(2j*np.pi*(fcenter+dnu)*t)
-            X=pfb(x,nframe,ntap,taper)
-            pos[i*100+j] = np.abs(X[fcenter+df])**2
+            x=np.exp(2j*np.pi*(fcenter+dnu)*t)      # sine wave
+            X=pfb(x,nframe,ntap,taper)              # PFB with optional taper
+            pos[i*100+j] = np.abs(X[fcenter+df])**2 # power
     return pos
 
 # helper, normalize to decibel scale
@@ -60,24 +55,25 @@ def decibel(y):
 
 
 pos1=get_pow(1024,np.ones)
-pos2=get_pow(1024,np.hamming)
-pos3=get_pow_pfb(1024,8,taper=np.hamming)
-pos4=get_pow_pfb(2048,4,taper=np.hamming)
+pos2=get_pow(1024,np.hanning)
+pos3=get_pow_pfb(1024,8,taper=np.hanning)
+pos4=get_pow_pfb(1024,4,taper=np.hanning)
+pos5=get_pow_pfb(1024,4,taper=None)
 
 print(f"Shapes: {pos1.shape}, {pos2.shape}, {pos3.shape}, {pos4.shape}")
 
 xl=np.linspace(-4,4,NX)
 plt.figure(0)
-plt.title('8 tap Hanning-PFB leakage comparison')
-plt.plot(xl,decibel(pos1),"--",label='No window',color=colors[0])
-plt.plot(xl,decibel(pos2),"-.",label='Hann only',color=colors[1])
-plt.plot(xl,decibel(pos3),label='Hann-PFB 8 Taps',color=colors[2])
-plt.plot(xl,decibel(pos4),label='Hann-PFB 4 Taps',color=colors[3])
-plt.legend()
+plt.title('PFB vs DFT filter-bank response in each band')
+plt.plot(xl,decibel(pos1),"--",label='FFT, no window',color=colors[0])
+plt.plot(xl,decibel(pos2),"-.",label='FFT, Hann window',color=colors[1])
+#plt.plot(xl,decibel(pos3),"-",label='PFB, 8 Taps, Hann window',color=colors[2])
+plt.plot(xl,decibel(pos5),"--",label='PFB, 4 Taps, no window',color=colors[2])
+plt.plot(xl,decibel(pos4),"-",label='PFB, 4 Taps, Hann window',color=colors[3])
+plt.legend(loc='lower left')
 plt.ylabel('Relative power (dB)',fontsize=12)
 plt.xlabel('Frequency offset (channel)',fontsize=12)
 plt.ylim(-80,1)
-
 plt.tight_layout()
 plt.savefig("img/sidelobes2.png",dpi=450)
 plt.show()
